35b74d56c6f13cb57927ed301230e45f2a304133
Sindhuja Addala <sindhu.a@saince.in>
2024-05-07 19:22:05 +0530

    expense-ansible

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,25 +1,36 @@
-#resource <resource-type> <resource-name>
-resource "aws_security_group" "allow_ssh {
-    name= "allow-ssh"
-    description = "Allowing ssh access"
+# resource <resource-type> <resource-name>
+resource "aws_instance" "db" {
+
+    ami = "ami-090252cbe067a9e58"
+    vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+    instance_type = "t3.micro"
+
+    tags = {
+        Name = "db"
+    }
+}
+
+resource "aws_security_group" "allow_ssh" {
+    name = "allow_ssh"
+    description = "allowing SSH access"
 
     ingress {
-        from_port        = 20
-        to_port          = 20
-        protocol         = "                                 "
+        from_port        = 22
+        to_port          = 22
+        protocol         = "tcp"
         cidr_blocks      = ["0.0.0.0/0"]
-        ipv6_cidr_blocks = ["::/0"]
     }
+
     egress {
-        from_port        = 0
+        from_port        = 0 # from 0 to 0 means, opening all protocols
         to_port          = 0
-        protocol         = "-1"
+        protocol         = "-1" # -1 all protocols
         cidr_blocks      = ["0.0.0.0/0"]
-        ipv6_cidr_blocks = ["::/0"]
     }
 
     tags = {
-        name = "allow_ssh"
-        createdby = "Sindhu"
+        Name = "allow_ssh"
+        CreatedBy = "sindhu"
     }
 }
+
